a97408866df2d41b698d37af699d7ae2512a70f3
Kasam Akshay <171951023+Ak47-max26@users.noreply.github.com>
2025-10-16 20:21:57 +0530

    Add files via upload

---
+++ schema/entitlement.go
@@ -0,0 +1,91 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	baseMixin "github.com/flexprice/flexprice/ent/schema/mixin"
+	"github.com/flexprice/flexprice/internal/types"
+)
+
+// Entitlement holds the schema definition for the Entitlement entity.
+type Entitlement struct {
+	ent.Schema
+}
+
+// Mixin of the Entitlement.
+func (Entitlement) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		baseMixin.BaseMixin{},
+		baseMixin.EnvironmentMixin{},
+	}
+}
+
+// Fields of the Entitlement.
+func (Entitlement) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			NotEmpty().
+			Unique().
+			Immutable(),
+		field.String("entity_type").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Default(string(types.ENTITLEMENT_ENTITY_TYPE_PLAN)).
+			Optional(),
+
+		field.String("entity_id").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Optional(),
+		field.String("feature_id").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			NotEmpty(),
+		field.String("feature_type").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			NotEmpty(),
+		field.Bool("is_enabled").
+			Default(false),
+		field.Int64("usage_limit").
+			Optional().
+			Nillable(),
+		field.String("usage_reset_period").
+			SchemaType(map[string]string{
+				"postgres": "varchar(20)",
+			}).
+			Optional(),
+		field.Bool("is_soft_limit").
+			Default(false),
+		field.String("static_value").
+			Optional(),
+		field.Int("display_order").
+			Default(0),
+	}
+}
+
+// Edges of the Entitlement.
+func (Entitlement) Edges() []ent.Edge {
+	return []ent.Edge{}
+}
+
+// Indexes of the Entitlement.
+func (Entitlement) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("tenant_id", "environment_id", "entity_type", "entity_id", "feature_id").
+			Unique().
+			Annotations(entsql.IndexWhere("status = 'published'")),
+
+		index.Fields("tenant_id", "environment_id", "entity_type", "entity_id"),
+		index.Fields("tenant_id", "environment_id", "feature_id"),
+	}
+}
---
+++ schema/feature.go
@@ -0,0 +1,94 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	baseMixin "github.com/flexprice/flexprice/ent/schema/mixin"
+)
+
+// Feature holds the schema definition for the Feature entity.
+type Feature struct {
+	ent.Schema
+}
+
+// Mixin of the Feature.
+func (Feature) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		baseMixin.BaseMixin{},
+		baseMixin.EnvironmentMixin{},
+	}
+}
+
+func (Feature) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Unique().
+			Immutable(),
+		field.String("lookup_key").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Immutable(),
+		field.String("name").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			NotEmpty(),
+		field.Text("description").
+			Optional().
+			Nillable(),
+		field.String("type").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			NotEmpty().
+			Immutable(),
+		field.String("meter_id").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Optional().
+			Nillable(),
+		field.JSON("metadata", map[string]string{}).
+			Optional().
+			SchemaType(map[string]string{
+				"postgres": "jsonb",
+			}),
+		field.String("unit_singular").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Optional().
+			Nillable(),
+		field.String("unit_plural").
+			SchemaType(map[string]string{
+				"postgres": "varchar(50)",
+			}).
+			Optional().
+			Nillable(),
+	}
+}
+
+// Indexes of the Feature.
+func (Feature) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("tenant_id", "environment_id", "lookup_key").
+			Unique().
+			StorageKey("idx_feature_tenant_env_lookup_key_unique").
+			Annotations(entsql.IndexWhere("(lookup_key IS NOT NULL AND lookup_key != '') AND status = 'published'")),
+		index.Fields("tenant_id", "environment_id", "meter_id").
+			StorageKey("idx_feature_tenant_env_meter_id").
+			Annotations(entsql.IndexWhere("meter_id IS NOT NULL")),
+		index.Fields("tenant_id", "environment_id", "type").
+			StorageKey("idx_feature_tenant_env_type"),
+		index.Fields("tenant_id", "environment_id", "status").
+			StorageKey("idx_feature_tenant_env_status"),
+		index.Fields("tenant_id", "environment_id", "created_at").
+			StorageKey("idx_feature_tenant_env_created_at"),
+	}
+}
